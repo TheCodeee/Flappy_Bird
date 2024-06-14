c05eea1b6106efc7e63e29e0564af682c6daa745
Farhan <farhanjamadar66@gmail.com>
2024-06-14 20:10:01 +0530

    late

--- main.py
+++ main.py
@@ -1,76 +1,107 @@
-# Importing the library
-import pygame
-import random
-import time
- 
-# Initializing Pygame modules
-pygame.init()
-
-# time
-clock = pygame.time.Clock()
-dt = clock.tick(120)
-
-#  Screen Vars
-x = 600
+x = 1200
 y = 900
 
-# Vars
-gameover = False
-running = True
-velo_max = 2
 
-backgr = pygame.image.load("bg.jpg")
-backgr = pygame.transform.scale(backgr, (1100, 900))
+# imports 
+import pygame 
+from random import randint
+        
 
-gr_pos = 0
-gr = pygame.image.load("ground.png")
-gr = pygame.transform.scale(gr, (1100, 300))
-
-bird_pos = 50
-bird = pygame.image.load("bird.png")
-bird = pygame.transform.scale(bird, (120, 80))
-
-
-# Initializing surface
-surface = pygame.display.set_mode((x, y))
- 
-# Initializing RGB Color
-blue = (0, 0, 255)
+# colors 
 white = (255, 255, 255)
- 
-# Changing surface color
-surface.fill(white)
+
+# Initialize 
+pygame.init()
+pygame.mixer.init()
 
 
-# LOOOOOP
+
+pipe_up = pygame.image.load("pipeup.png")
+pipe_up = pygame.transform.scale(pipe_up, (100, 500))
+pipe_down = pygame.image.load("pipedown.png")
+pipe_down = pygame.transform.scale(pipe_down, (100, 500))
+
+
+# images
+background = pygame.image.load("bg.jpg")
+background = pygame.transform.scale(background, (x, y))
+
+
+ground = pygame.image.load("ground.png")
+ground = pygame.transform.scale(ground, (1300, 225))
+
+
+bird = pygame.image.load("bird.png")
+bird = pygame.transform.scale(bird, (80, 60))
+
+
+# Variables 
+pipe_counter = 0
+bird_velo = 0
+bird_accn = 1
+bird_velo_max = 5
+bird_pos = 200
+ground_pos = 0
+ground_velocity = -5
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((x, y))
+running = True
+pipe_y = randint(-400, -100)
+pipe_x = 1200
+
+
+# Loop
 while running:
-    # GAME PHYSICS
-    gravity = 1
-    velo_y = 4
+
+    pipe_counter += 1
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                while velo_y == velo_max:
-                    bird_pos -= velo_y
-                    velo_y += gravity
-    
-    # BACKGROUND 
-    surface.blit(backgr, (0, 0))
-    surface.blit(gr, (gr_pos, 650))
-    gr_pos -= 1
-    if gr_pos == (-490):
-        gr_pos = 0
 
-    # BIRD Handeling 
-    bird_pos += velo_max
-    surface.blit(bird, (80, bird_pos))
-    if bird_pos >= 600:
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                bird_velo = -12
+                bird_accn = 1
+                pygame.mixer.music.load("flap.mp3")
+                pygame.mixer.music.play()
+
+
+    bird_velo += bird_accn
+
+
+    if bird_velo == bird_velo_max:
+        bird_accn = 0
+
+
+    bird_pos += bird_velo
+
+
+    ground_pos += ground_velocity
+
+
+    if ground_pos == -60:
+        ground_pos = 0
+
+
+    if bird_pos >= 635 or bird_pos <= 0:
         running = False
-    clock.tick(120)
+
+
+    screen.fill((white))
+
+    screen.blit(background, (0, 0))
+
+    if pipe_counter == 120:
+        pipe_counter = 0
+    pipe_x += ground_velocity
+    screen.blit(pipe_down, (pipe_x, pipe_y))
+    screen.blit(pipe_up, (pipe_x, pipe_y + 700))
+        
+    screen.blit(ground, (ground_pos, 675))
+    screen.blit(bird, (500, bird_pos))
 
     pygame.display.update()
 
 pygame.quit()
-
