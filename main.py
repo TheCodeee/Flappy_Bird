0fbaacabd9cef8326db2271d56e98198925066a1
TheCodeee <farhanjamadar66@gmail.com>
2024-08-18 14:17:29 +0530

    main

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import pygame as pg
 from random import randint
 
 pg.init()
+clock = pg.time.Clock()
 
 class game:
     def __init__(self):
         # Game Variables
         self.groundx = 0
+        self.groundy = 710
         self.screenx = 1600
         self.screeny = 970
         self.gameover = False
+
+        # bird Variables 
+        self.accn = 2
+        self.terminalVelo = 10
+        self.bird_velo = 0
 
         # gameSprites 
         self.birdx = self.screenx/2 - 100
         self.birdy = self.screeny/3
         self.bird = pg.image.load("bird.png")
-        self.bird = pg.transform.scale(self.bird, (self.screenx/11, self.screeny/11))
+        self.bird = pg.transform.scale(self.bird, (self.screenx/14, self.screeny/15))
         self.bg = pg.image.load("bg.jpg")
         self.bg = pg.transform.scale(self.bg, (self.screenx, self.screeny))
         self.ground = pg.image.load("ground.png")
@@ -26,15 +33,6 @@ class game:
     def pipes(self):
         pass
 
-    def Flap(self):
-        self.accn = 4
-        self.terminalVelo = 10
-        self.bird_velo = 0
-        self.birdy += self.bird_velo
-        if self.bird_velo > self.terminalVelo:
-            self.bird_velo = self.terminalVelo
-        self.bird_velo += self.accn
-
 play = game()
 
 # colors
@@ -44,23 +42,31 @@ white = (255, 255, 255)
 screen = pg.display.set_mode((play.screenx, play.screeny))
 pg.display.set_caption("Flappy Bird")
 
-while not play.gameover:
-    play.Flap()
+while not play.gameover:        
     for event in pg.event.get():
         if event.type == pg.QUIT:
             play.gameover = True
 
         if event.type == pg.KEYDOWN:
-            if event.type == pg.K_SPACE:
-                play.bird_velo = -15
-    
+            if event.key == pg.K_SPACE:
+                play.bird_velo = -25
+
+    play.birdy += play.bird_velo
+    if play.bird_velo > play.terminalVelo: 
+        play.bird_velo = play.terminalVelo
+    play.bird_velo += play.accn
+
+    if play.birdy < 0 or play.birdy > play.groundy:
+        play.gameover = True 
+
     play.groundx -= 5
     if play.groundx <= -80:
         play.groundx = 0
 
     screen.blit(play.bg, (0, 0))
-    screen.blit(play.ground, (play.groundx, 710))
+    screen.blit(play.ground, (play.groundx, play.groundy))
     screen.blit(play.bird, (play.birdx, play.birdy))
 
+    clock.tick(60)
     pg.display.update()
 pg.quit()
